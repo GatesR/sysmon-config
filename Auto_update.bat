b4e544563cee5ea90167e20552dcfe1dc4b1ec70
R gates <34067272+GatesR@users.noreply.github.com>
2017-11-28 10:38:00 -0500

    Update Auto_update.bat

--- Auto_update.bat
+++ Auto_update.bat
@@ -1,5 +1,5 @@
 @echo on
-cd C:\ProgramData\sysmon\
+cd C:\ProgramData\SMon\
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/gatesr/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-mon64 -c sysmonconfig-export.xml
+SMon64 -c sysmonconfig-export.xml
 exit
