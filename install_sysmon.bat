13faaa4ba21afdff2d1224a72ed09801486010ef
R gates <34067272+GatesR@users.noreply.github.com>
2017-11-28 10:37:28 -0500

    Update install_sysmon.bat

--- install_sysmon.bat
+++ install_sysmon.bat
@@ -12,23 +12,23 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
-mkdir C:\ProgramData\sysmon
-pushd "C:\ProgramData\sysmon\"
-echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\mon64.exe')"
+mkdir C:\ProgramData\SMon
+pushd "C:\ProgramData\SMon\"
+echo [+] Downloading SMon...
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\SMon64.exe')"
 echo [+] Downloading Sysmon config...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/gatesr/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/gatesr/sysmon-config/master/Auto_update.bat','C:\ProgramData\sysmon\Auto_Update.bat')"
-sysmon64.exe -accepteula -i sysmonconfig-export.xml
-echo [+] Sysmon Successfully Installed!
-attrib +s +h +r c:\ProgramData\sysmon
-echo Y | cacls c:\ProgramData\Sysmon /e /p everyone:n
-echo Y | cacls c:\ProgramData\Sysmon /p system:f
-echo Y | cacls c:\ProgramData\Sysmon /p Administrators:f
-sc failure Sysmon actions= restart/10000/restart/10000// reset= 120
-echo [+] Sysmon Directory Permissions Reset and Services Hidden
-sc sdset Sysmon D:(D;;DCLCWPDTSD;;;IU)(D;;DCLCWPDTSD;;;SU)(D;;DCLCWPDTSD;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)
+SMon64.exe -accepteula -i sysmonconfig-export.xml
+echo [+] mon Successfully Installed!
+attrib +s +h +r c:\ProgramData\mon
+echo Y | cacls c:\ProgramData\SMon /e /p everyone:n
+echo Y | cacls c:\ProgramData\SMon /p system:f
+echo Y | cacls c:\ProgramData\SMon /p Administrators:f
+sc failure mon actions= restart/10000/restart/10000// reset= 120
+echo [+] SMon Directory Permissions Reset and Services Hidden
+sc sdset SMon D:(D;;DCLCWPDTSD;;;IU)(D;;DCLCWPDTSD;;;SU)(D;;DCLCWPDTSD;;;BA)(A;;CCLCSWLOCRRC;;;IU)(A;;CCLCSWLOCRRC;;;SU)(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)S:(AU;FA;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;WD)
 echo [+] Creating Auto Update Task set to Hourly..
-SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update.bat /F /ST %tasktime%
+SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_SMon_Rules /TR C:\ProgramData\mon\Auto_Update.bat /F /ST %tasktime%
 timeout /t 10
 exit
